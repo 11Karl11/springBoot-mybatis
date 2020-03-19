549708d3a3c814c29647c4971f3d79f7e6f29c9e
khk <46013769+11Karl11@users.noreply.github.com>
2020-03-19 14:54:53 +0800

    修改redis地址

--- src/main/java/com/hangz/spring/mybatis/SpringMybatisApplication.java
+++ src/main/java/com/hangz/spring/mybatis/SpringMybatisApplication.java
@@ -3,8 +3,9 @@ package com.hangz.spring.mybatis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-
+@EnableCaching
 @MapperScan("com.hangz.spring.mybatis.mapper")
 @SpringBootApplication
 public class SpringMybatisApplication {
